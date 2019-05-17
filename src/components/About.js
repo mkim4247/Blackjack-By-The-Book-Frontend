4c2cc9095592f03d3d359c471f0ddab8d974ac46
Michael Kim <m.kim4247@gmail.com>
2019-05-16 22:23:52 -0400

    fixed small about style

--- src/components/About.js
+++ src/components/About.js
@@ -56,9 +56,9 @@ class About extends React.Component {
                   OpenGameArt - koralheim
                 </a>
               </li>
-            </ul>
-            <ul>
-              Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+              <li>
+                Ace Card Icon made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+              </li>
             </ul>
           </div>
         </div>
