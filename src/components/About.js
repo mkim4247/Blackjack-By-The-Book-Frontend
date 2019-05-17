628f76e7c7395fa9a6c89813bb5f40db4421e5f1
Michael Kim <m.kim4247@gmail.com>
2019-05-16 22:19:11 -0400

    changed icon on browser

--- src/components/About.js
+++ src/components/About.js
@@ -57,6 +57,9 @@ class About extends React.Component {
                 </a>
               </li>
             </ul>
+            <ul>
+              Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+            </ul>
           </div>
         </div>
       </div>
