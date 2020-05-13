5db9c9e32a4b035060818d501cbfadc98f7be349
Michael Kim <m.kim4247@gmail.com>
2020-05-12 22:14:42 -0400

    starting refactor

--- src/components/Bet.js
+++ src/components/Bet.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { placingBet } from '../redux/actions'
 
-class Bet extends React.Component {
-  increaseBet = event => {
+const Bet = props => {
+  const increaseBet = event => {
     let amount = parseInt(event.currentTarget.value);
-    if(this.props.user.pot >= amount){
-      this.props.placingBet(amount)
+    if(props.user.pot >= amount){
+      props.placingBet(amount)
     }
     else {
       return(
@@ -17,71 +17,69 @@ class Bet extends React.Component {
     }
   }
 
-  render(){
-    return(
-      <div id='bet-box'>
-        {this.props.roundResult !== "Deal" ?
+  return(
+    <div id='bet-box'>
+      {props.roundResult !== "Deal" ?
+        <div>
           <div>
-            <div>
-              {this.props.user.pot >= 1 ?
-                <input
-                  className='betting-chips'
-                  type="image"
-                  src={require('../images/greenchip.svg')}
-                  onClick={this.increaseBet}
-                  value='1'
-                  alt='Green Chip/1'/>
-                : null
-              }
-              {this.props.user.pot >=5 ?
-                <input
-                  className='betting-chips'
-                  type="image"
-                  src={require('../images/redchip.svg')}
-                  onClick={this.increaseBet}
-                  value='5'
-                  alt='Red Chip/5'/>
-                : null
-              }
-            </div>
-            <div>
-              {this.props.user.pot >= 25 ?
-                <input
-                  className='betting-chips'
-                  type="image"
-                  src={require('../images/lightbluechip.svg')}
-                  onClick={this.increaseBet}
-                  value='25'
-                  alt='Light Blue Chip/25'/>
-                : null
-              }
-              {this.props.user.pot >= 100 ?
-                <input
-                  className='betting-chips'
-                  type="image"
-                  src={require('../images/blackchip.svg')}
-                  onClick={this.increaseBet}
-                  value='100'
-                  alt='Black Chip/100'/>
-                : null
-              }
-            </div>
-            {this.props.user.pot > 1 ?
+            {props.user.pot >= 1 ?
               <input
                 className='betting-chips'
                 type="image"
-                src={require('../images/whitechip.svg')}
+                src={require('../images/greenchip.svg')}
                 onClick={this.increaseBet}
-                value={this.props.user.pot}
-                alt='White Chip/All in'/>
+                value='1'
+                alt='Green Chip/1'/>
+              : null
+            }
+            {props.user.pot >=5 ?
+              <input
+                className='betting-chips'
+                type="image"
+                src={require('../images/redchip.svg')}
+                onClick={this.increaseBet}
+                value='5'
+                alt='Red Chip/5'/>
               : null
             }
           </div>
-          : null
-        }
-      </div>
-    )
-  }
+          <div>
+            {props.user.pot >= 25 ?
+              <input
+                className='betting-chips'
+                type="image"
+                src={require('../images/lightbluechip.svg')}
+                onClick={this.increaseBet}
+                value='25'
+                alt='Light Blue Chip/25'/>
+              : null
+            }
+            {props.user.pot >= 100 ?
+              <input
+                className='betting-chips'
+                type="image"
+                src={require('../images/blackchip.svg')}
+                onClick={this.increaseBet}
+                value='100'
+                alt='Black Chip/100'/>
+              : null
+            }
+          </div>
+          {props.user.pot > 1 ?
+            <input
+              className='betting-chips'
+              type="image"
+              src={require('../images/whitechip.svg')}
+              onClick={this.increaseBet}
+              value={props.user.pot}
+              alt='White Chip/All in'/>
+            : null
+          }
+        </div>
+        : null
+      }
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
--- src/components/DealerHand.js
+++ src/components/DealerHand.js
@@ -1,41 +1,40 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-class DealerHand extends React.Component {
-  render(){
-    return(
-      <div id='dealer-hand'>
-        {this.props.dealerHand.cards.length > 0 && this.props.showDealer ?
-          this.props.dealerHand.cards.map( (card, index) => {
-            return <img
-              key={index}
-              id={`dealerCard-${index}`}
+const DealerHand = props => {
+  return(
+    <div id='dealer-hand'>
+      {props.dealerHand.cards.length > 0 && props.showDealer ?
+        props.dealerHand.cards.map( (card, index) => {
+          return <img
+            key={index}
+            id={`dealerCard-${index}`}
+            className='cards'
+            src={card.image}
+            alt={card.value}
+            />
+          }
+        )
+        :
+        props.dealerHand.cards.length > 0 ?
+          <div>
+            <img
+              id='dealerCard-0'
               className='cards'
-              src={card.image}
-              alt={card.value}
-              />
-            }
-          )
-          :
-          this.props.dealerHand.cards.length > 0 ?
-            <div>
-              <img
-                id='dealerCard-0'
-                className='cards'
-                src={this.props.dealerHand.cards[0].image}
-                alt={this.props.dealerHand.cards[0].value}
-              />
-              <img
-                id="facedown"
-                className='cards' src='https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-1_grande.png?v=1535755695'
-                alt='facedown'
-              />
-            </div>
-            : null
-        }
-      </div>
-    )
-  }
+              src={props.dealerHand.cards[0].image}
+              alt={props.dealerHand.cards[0].value}
+            />
+            <img
+              id="facedown"
+              className='cards'
+              src='https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-1_grande.png?v=1535755695'
+              alt='facedown'
+            />
+          </div>
+        : null
+      }
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
